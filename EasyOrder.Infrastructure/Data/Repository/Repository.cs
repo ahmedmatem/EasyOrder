7858fcde13eec85c002f074ba868836d1539558f
Ahmed Matem <ahmedmatem@gmail.com>
2025-01-15 20:58:17 +0200

    Create Repository

---
+++ EasyOrder.Infrastructure/Data/Repository/Repository.cs
@@ -0,0 +1,168 @@
+﻿namespace EasyOrder.Infrastructure.Data.Repository
+{
+    using System.Linq.Expressions;
+
+    using Microsoft.EntityFrameworkCore.ChangeTracking;
+    using Microsoft.EntityFrameworkCore;
+
+    internal class Repository : IRepository
+    {
+        private readonly DbContext context;
+        private DbSet<T> DbSet<T>() where T : class => context.Set<T>();
+
+        public Repository(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        /// <summary>
+        /// All records in a table
+        /// </summary>
+        /// <returns>Queryable expression tree</returns>
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> All<T>(Expression<Func<T, bool>> search) 
+            where T : class
+        {
+            return this.DbSet<T>().Where(search);
+        }
+
+        /// <summary>
+        /// The result collection won't be tracked by the context
+        /// </summary>
+        /// <returns>Expression tree</returns>
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> AllReadonly<T>(Expression<Func<T, bool>> search) 
+            where T : class
+        {
+            return this.DbSet<T>()
+                .Where(search)
+                .AsNoTracking();
+        }
+
+        /// <summary>
+        /// Adds entity to the database
+        /// </summary>
+        /// <param name="entity">Entity to add</param>
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await context.AddAsync<T>(entity);
+        }
+
+        public async Task AddRangeAsync<T>(IEnumerable<T> entities)
+            where T : class
+        {
+            await DbSet<T>().AddRangeAsync(entities);
+        }
+
+        /// <summary>
+        /// Updates a record in database
+        /// </summary>
+        /// <param name="entity">Entity for record to be updated</param>
+        public void Update<T>(T entity) where T : class
+        {
+            this.DbSet<T>().Update(entity);
+        }
+
+        /// <summary>
+        /// Updates set of records in the database
+        /// </summary>
+        /// <param name="entities">Enumerable collection of entities to be updated</param>
+        public void UpdateRange<T>(IEnumerable<T> entities) where T : class
+        {
+            this.DbSet<T>().UpdateRange(entities);
+        }
+
+        /// <summary>
+        /// Deletes a record from database
+        /// </summary>
+        /// <param name="id">Identificator of record to be deleted</param>
+        public async Task DeleteAsync<T>(object id) where T : class
+        {
+            T? entity = await GetByIdAsync<T>(id);
+            if (entity != null)
+            {
+                Delete<T>(entity);
+            }
+        }
+
+        /// <summary>
+        /// Deletes a record from database
+        /// </summary>
+        /// <param name="entity">Entity representing record to be deleted</param>
+        public void Delete<T>(T entity) where T : class
+        {
+            EntityEntry entry = context.Entry(entity);
+
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbSet<T>().Attach(entity);
+            }
+
+            entry.State = EntityState.Deleted;
+        }
+
+        public void DeleteRange<T>(IEnumerable<T> entities) where T : class
+        {
+            this.DbSet<T>().RemoveRange(entities);
+        }
+
+        public void DeleteRange<T>(Expression<Func<T, bool>> deleteWhereClause) where T : class
+        {
+            var entities = All<T>(deleteWhereClause);
+            DeleteRange(entities);
+        }
+
+        /// <summary>
+        /// Detaches given entity from the context
+        /// </summary>
+        /// <param name="entity">Entity to be detached</param>
+        public void Detach<T>(T entity) where T : class
+        {
+            EntityEntry entry = context.Entry(entity);
+
+            entry.State = EntityState.Detached;
+        }
+
+        /// <summary>
+        /// Disposing the context when it is not neede
+        /// Don't have to call this method explicitely
+        /// Leave it to the IoC container
+        /// </summary>
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        /// <summary>
+        /// Saves all made changes in trasaction
+        /// </summary>
+        /// <returns>Error code</returns>
+        public async Task<int> SaveChangesAsync<T>() where T : class
+        {
+            return await context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Gets specific record from database by primary key
+        /// </summary>
+        /// <param name="id">record identificator</param>
+        /// <returns>Single record</returns>
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            return await DbSet<T>().FindAsync(id);
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object[] id) where T : class
+        {
+            return await DbSet<T>().FindAsync(id);
+        }
+    }
+}
