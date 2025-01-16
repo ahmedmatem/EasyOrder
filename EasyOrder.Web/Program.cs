4882390039e1e1848f30be9513131843346927be
Ahmed Matem <ahmedmatem@gmail.com>
2025-01-16 21:11:16 +0200

    Add Identity scaffolding item

--- EasyOrder.Web/Program.cs
+++ EasyOrder.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using EasyOrder.Infrastructure.Data;
 namespace EasyOrder.Web
 {
     using Microsoft.AspNetCore.Identity;
