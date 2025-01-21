d9d091a52dbb7bae045409bdc90927188ff9aaa8
Ahmed Matem <ahmedmatem@gmail.com>
2025-01-21 09:31:33 +0200

    Create IdentityRole configuration file

---
+++ EasyOrder.Infrastructure/Data/Configurations/IdentityRoleConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace EasyOrder.Infrastructure.Data.Configurations
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class IdentityRoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public static readonly IList<IdentityRole> roles = new List<IdentityRole>();
+
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            SeedRoles();
+            builder.HasData(roles);
+        }
+
+        private void SeedRoles()
+        {
+            roles.Add(new IdentityRole()
+            {
+                Name = "SupperAdmin",
+                NormalizedName = "SupperAdmin".ToUpper(),
+            });
+        }
+    }
+}
